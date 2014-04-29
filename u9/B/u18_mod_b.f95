ad147889f0e0af4b4644bc9a58c03677d892d777
Sebastian Domaschke <s5894525@NPLP24.pool.math.tu-dresden.de>
2014-04-29 14:36:49 +0200

    ferti a18 pointer

--- u9/B/u18_mod_b.f95
+++ u9/B/u18_mod_b.f95
@@ -10,33 +10,35 @@ MODULE u18_module
   ! Listenpointer
   TYPE :: CYCLE
      TYPE(Node), POINTER  :: Top
+!hier koennte man durch das typenkonstrukt noch eine zusaetzliche Variable einfuehren. Z.B. die Laenge der Liste, welche immer um eins erhoehte wird wenn ein neuer Eintrag allociert wird. So spart man viel Rechenzeit bei der Berechnung der Listenlaenge.
   END TYPE CYCLE
 
-  INTERFACE init_cycle
-     MODULE PROCEDURE init_cycle
-  END INTERFACE init_cycle
+  ! INTERFACE init_cycle
+  !    MODULE PROCEDURE init_cycle
+  ! END INTERFACE init_cycle
   
-  INTERFACE put_cycle
-     MODULE PROCEDURE put_cycle
-  END INTERFACE put_cycle
+  ! INTERFACE put_cycle
+  !    MODULE PROCEDURE put_cycle
+  ! END INTERFACE put_cycle
 
-  INTERFACE last_one
-     MODULE PROCEDURE last_one
-  END INTERFACE last_one
+  ! INTERFACE last_one
+  !    MODULE PROCEDURE last_one
+  ! END INTERFACE last_one
 
-  INTERFACE del_next
-     MODULE PROCEDURE del_next
-  END INTERFACE del_next
+  ! INTERFACE del_next
+  !    MODULE PROCEDURE del_next
+  ! END INTERFACE del_next
   
-  INTERFACE build_cycle
-     MODULE PROCEDURE build_cycle
-  END INTERFACE build_cycle
+  ! INTERFACE build_cycle
+  !    MODULE PROCEDURE build_cycle
+  ! END INTERFACE build_cycle
 
 CONTAINS
   
   ! Initialisierung der Liste
   SUBROUTINE init_cycle(c)
     TYPE(CYCLE), INTENT (out) :: c
+! Dadurch wird nicht mehr auf Speicher gezeigt
     NULLIFY(c%top)
   END SUBROUTINE init_cycle
   
@@ -47,12 +49,11 @@ CONTAINS
     last_one=.FALSE.
     IF(ASSOCIATED(c%top,c%top%next)) last_one=.TRUE.
   END FUNCTION last_one
-  
 
-  FUNCTION build_cycle (fname)
+ subroutine build_cycle (fname,ptr)
     implicit none
     character(*) :: fname
-    TYPE(CYCLE) :: tempp, build_cycle
+    TYPE(CYCLE) :: tempp, ptr
     INTEGER :: talter,ios
     CHARACTER(10) :: tname
     
@@ -62,9 +63,9 @@ CONTAINS
        STOP
     END IF
 
-    CALL init_cycle(build_cycle)
-    ALLOCATE (Build_cycle%top)
-    TemPp%top => Build_cycle%top
+    CALL init_cycle(ptr)
+    ALLOCATE (Ptr%top)
+    TemPp%top => Ptr%top
     READ (34,*,iostat=ios) TemPp%top%Name, TemPp%top%Alter
     DO
        ALLOCATE (TemPp%top%Next)      ! Neuen Knoten erstellen und verlinken
@@ -75,8 +76,8 @@ CONTAINS
        Tempp%top%Alter=talter
     END DO
     DEALLOCATE (Tempp%top%Next)
-    Tempp%top%Next => build_cycle%top ! Letzter Eintrag Zeigt auf den Kopf
-  END FUNCTION build_cycle
+    Tempp%top%Next => ptr%top ! Letzter Eintrag Zeigt auf den Kopf
+  END subroutine build_cycle
 
   ! Ausgaberoutine
   SUBROUTINE put_cycle(ptr)
@@ -88,6 +89,7 @@ CONTAINS
        neext => neext%next
     END DO
   END SUBROUTINE put_cycle
+
   SUBROUTINE del_next(c)
     TYPE(CYCLE) :: c, temp
     temp%top => c%top%next%next
